4b60da420174f3218cbb2c512f3c3af81f537be9
Jose <jgh9094@gmail.com>
2023-08-14 16:41:41 -0700

    forgot brackets when data appending

--- DataTools/Collector/Param_Sweep/over-time.py
+++ DataTools/Collector/Param_Sweep/over-time.py
@@ -85,8 +85,8 @@ def CheckDir(dir,dump,sch,offs,val):
                     params.UNI_STR_POS:   pd.Series(df[params.UNI_STR_POS].tolist()),
                     params.ELE_BIG_PEAK:   pd.Series(df[params.ELE_BIG_PEAK].tolist()),
                     params.ELE_STK_CNT:   pd.Series(df[params.ELE_STK_CNT].tolist()),
-                    params.GetSchemeParam(sch):pd.Series(params.GetSchemeParamSet(sch)[int(((s % params.GetSchemeSeedBound(sch)))/params.REPLICATES)] * len(GEN_LIST)),
-                    'acro':pd.Series(params.GetDiagnosticAcro(i) * len(GEN_LIST))
+                    params.GetSchemeParam(sch):pd.Series([params.GetSchemeParamSet(sch)[int(((s % params.GetSchemeSeedBound(sch)))/params.REPLICATES)]] * len(GEN_LIST)),
+                    'acro':pd.Series([params.GetDiagnosticAcro(i)] * len(GEN_LIST))
                 }))
 
             else:
@@ -100,8 +100,8 @@ def CheckDir(dir,dump,sch,offs,val):
                     params.OVERLAP:       pd.Series(df[params.OVERLAP].tolist()),
                     params.UNI_STR_POS:   pd.Series(df[params.UNI_STR_POS].tolist()),
                     params.ELE_STK_CNT:   pd.Series(df[params.ELE_STK_CNT].tolist()),
-                    params.GetSchemeParam(sch):pd.Series(params.GetSchemeParamSet(sch)[int(((s % params.GetSchemeSeedBound(sch)))/params.REPLICATES)] * len(GEN_LIST)),
-                    'acro':pd.Series(params.GetDiagnosticAcro(i) * len(GEN_LIST))
+                    params.GetSchemeParam(sch):pd.Series([params.GetSchemeParamSet(sch)[int(((s % params.GetSchemeSeedBound(sch)))/params.REPLICATES)]] * len(GEN_LIST)),
+                    'acro':pd.Series([params.GetDiagnosticAcro(i)] * len(GEN_LIST))
                 }))
 
     print('SAVING DATA NOW...')
